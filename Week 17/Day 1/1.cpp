64b14bdf7877362873cb2d408037a0ae192265fe
Sifat-00 <sifatbindoha123@gmail.com>
2025-02-23 23:25:05 +0600

    Update 1.cpp

--- Week 17/Day 1/1.cpp
+++ Week 17/Day 1/1.cpp
@@ -1,29 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+ 
+int main() 
 {
-    int t; 
+    int t;
     cin >> t;
-    while (t--)
+ 
+    while (t--) 
     {
-        int n;  
-        cin >> n;
-        string str1, str2;  
-        cin >> str1 >> str2;
-        
-        int one = 0, count = 0, one2 = 0;
-        
-        for (int i = 0; i < n; i++) 
+        int n, k;
+        cin >> n >> k;
+ 
+        string s;
+        char x = 97 ;
+        while(k--)
         {
-            if (str1[i] == '1') one++;
-            if (str2[i] == '1') one2++;
-            if (str2[i] != str1[i] && str2[i] == '1') count++;
+            s+=x;
+            x++;
         }
-        
-        int ans = count;
-        
-        if (one > one2) ans += (one - one2);
-        
+        string ans;
+        for(int i=0;i<n;i++)
+        {
+            ans+=s;
+        }
         cout << ans << endl;
     }
+ 
+    return 0;
 }
